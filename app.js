5b40b17bee537e67c1deeb2186f55a7ccccef67f
Luis <luisbarrios0110@gmail.com>
2025-08-22 15:12:01 -0500

    Fix: Reparar el fallo en el boton de pausa, Feat: nuevo estilos para la UI general, Fix: Igualar diferencias de tiempos en carga automatica,

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
+/* Variables Generales */
+const beep = new Audio("/assets/sounds/beep.mp3");
 let workTime, restTime, rounds;
 let isWorking = true;
 let timer = null;
 let remaining = 0;
 let currentRound = 1;
 const max = 283;
-const beep = new Audio("/assets/sounds/beep.mp3");
 
+/* Variables de Display */
 const countdownEl = document.getElementById("countdown");
 const modeEl = document.getElementById("mode");
 const nextmode = document.querySelector('#n-mode');
 const numRound = document.querySelector('#n-round');
 const circuloAmutar = document.querySelector("#aumCirculo");
-const boxdialog = document.querySelector("#dialog-timer");
 
-document.getElementById("startBtn").addEventListener("click", startTimer);
-document.getElementById("pauseBtn").addEventListener("click", pauseTimer);
-document.getElementById("resetBtn").addEventListener("click", resetTimer);
+/* Variables de botones */
+const btnStart = document.getElementById("startBtn");
+const btnPause = document.getElementById("pauseBtn");
+const btnReset = document.getElementById("resetBtn");
 
-const btnSave = document.querySelector('#save-data').addEventListener('click', () => { 
-  resetTimer();
-  const min = String(Math.floor(parseInt(document.getElementById("inpWorkTime").value) / 60));
-  const sec = String(parseInt(document.getElementById("inpWorkTime").value) % 60).padStart(2, "0");
-  countdownEl.textContent = `${min}:${sec}`;
-  numRound.textContent = `${currentRound}/${parseInt(document.getElementById("iptRounds").value)}`;
-  boxdialog.close();
-})
+btnStart.addEventListener("click", startTimer);
+btnPause.addEventListener("click", pauseTimer);
+btnReset.addEventListener("click", resetTimer);
 
 function startTimer() {
     
-  workTime = parseInt(document.getElementById("inpWorkTime").value);
-  restTime = parseInt(document.getElementById("inpRestTime").value);
-  rounds = parseInt(document.getElementById("iptRounds").value); 
-  circuloAmutar.setAttribute("stroke-dashoffset", max);
-  remaining = isWorking ? workTime : restTime;
-  beep.play();
-  if (timer === null && currentRound === 1 && remaining === 1) {
-  } 
+  if ( remaining === 0) {
+    workTime = parseInt(document.getElementById("inpWorkTime").value);
+    restTime = parseInt(document.getElementById("inpRestTime").value);
+    rounds = parseInt(document.getElementById("iptRounds").value); 
+    remaining = isWorking ? workTime : restTime;  
+    circuloAmutar.setAttribute("stroke-dashoffset", max);
+    beep.play();
+  }
 
   if (!timer) {
-    updateDisplay();
     timer = setInterval(tick, 1000);
   }
+
+  btnStart.style.display = ("none");
+  btnPause.classList.remove("hidden");
 }
 
 function pauseTimer() {
   clearInterval(timer);
   timer = null;
+  btnStart.style.display = ("flex");
+  btnPause.classList.add("hidden");
 }
 
 function resetTimer() {
   pauseTimer();
   isWorking = true;
   currentRound = 1;
+  remaining = 0;
   modeEl.textContent = "Listo";
   countdownEl.textContent = "00:00";
   circuloAmutar.setAttribute("stroke-dashoffset",max);
@@ -65,12 +67,11 @@ function tick() {
   // Aviso previo de 5 segundos
   if (remaining === 3 || remaining === 2 || remaining === 1) {
     beep.play();
-    if (navigator.vibrate) navigator.vibrate(500);
-  }
+   }
 
   if (remaining <= 0) {
     beep.play();
-    if (navigator.vibrate) navigator.vibrate([300, 200, 300]);
+     
     if (isWorking) {
       isWorking = false;
       remaining = restTime;
@@ -96,7 +97,6 @@ function tick() {
 }
 
 function updateDisplay() {
-  /* modeEl.textContent = isWorking ? `Trabajo` : `Descanso`; */
   if (isWorking) {
     modeEl.style.color = 'var(--verdoso)';
     modeEl.textContent = `Trabajo`;
@@ -122,9 +122,14 @@ function updateBar() {
   circuloAmutar.setAttribute("stroke-dashoffset",avanceFinal)
 }
 
-
-/* BOTON DIALOG */
-const btnDialog = document.querySelector('#show-dialog');
-btnDialog.addEventListener('click', () => {
-  boxdialog.showModal();
+// ------------- EVENTOS BOTONES COMPLEMENTARIOS---------------- //
+  
+/* BOTON ACTUALIZAR DISPLAY*/
+const btnSave = document.querySelector('#save-data').addEventListener('click', () => { 
+  resetTimer();
+  const min = String(Math.floor(parseInt(document.getElementById("inpWorkTime").value) / 60)).padStart(2, "0");
+  const sec = String(parseInt(document.getElementById("inpWorkTime").value) % 60).padStart(2, "0");
+  countdownEl.textContent = `${min}:${sec}`;
+  numRound.textContent = `${currentRound}/${parseInt(document.getElementById("iptRounds").value)}`;
+  document.querySelector("#dialog-timer").close();
 })
