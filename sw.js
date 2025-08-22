054047e22799af6ac9feb26839ef2a870ffdd463
Luis <luisbarrios0110@gmail.com>
2025-08-22 16:00:40 -0500

    Fix: ajustes de rutas de archivos en el SW

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = "interval-timer-v1";
+const CACHE_NAME = "cronofit-timer-v1";
 const ASSETS = [
   "/",
-  "/assets",
-  "/assets/images",
-  "/assets/sounds",
-  "/index.html",
   "/style.css",
   "/app.js",
-  "/manifest.json",
+  "/assets/icons/menu.svg",
+  "/assets/icons/pause.svg",
+  "/assets/icons/play.svg",
+  "/assets/icons/plus.svg",
+  "/assets/icons/reload.svg",
+  "/assets/icons/seting.svg",
+  "/favicon/favicon.ico",
   "/assets/sounds/beep.mp3"
 ];
 
@@ -16,6 +18,20 @@ self.addEventListener("install", e => {
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then( (cachesNames) => {
+      return Promise.all(
+        cachesNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  );
+})
 
 self.addEventListener("fetch", e => {
   e.respondWith(
